1561dadaf8f2bb99a40bfab2d3790fe80917cf87
Luca Pastori <luca.pastori@studenti.unipr.it>
2021-02-15 22:28:07 +0100

    ipfs setup react and ui now only upload
    Co-authored-by: Filippo SCARAMUZZA <filippo.scaramuzza@studenti.unipr.it>

--- test_ipfs_react/src/App.js
+++ test_ipfs_react/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+
+    let colors = ["red", "yellow", "orange", "olive", "green", "teal", "blue", "violet", "purple", "pink", "brown", "grey"];
+    let randIcon = colors[Math.floor(Math.random() * colors.length)] + " ethereum icon";
+    //let randButton = "ui " + colors[Math.floor(Math.random() * colors.length)] + " right labeled icon button";
+    let randButton = "ui teal right labeled icon button";
+
+    return (
+      <div className="App padded" style={{ margin: "20px", textAlign: "center" }}>
+        <h2 className="ui center aligned icon header">
+          <i className={randIcon}></i>
+        Withdraw from Cloudest Faucet
+        </h2>
+        <div className="sub header">The Cloud We Deserve<span role="img" aria-label="heart">❤️</span></div><br />
+        <div className="sub header">Ether available on the Faucet</div>
+        <div className="sub header">Ether owned by you</div>
+
+        <div className="ui action input" style={{ margin: "20px" }}>
+          <input type="text" name="amountRequested" />
+          <button className={randButton} >
+            <i className="cart plus icon"></i>
+            Withdraw
+        </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
