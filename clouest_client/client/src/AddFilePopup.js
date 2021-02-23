59df6eff18d5145576388f563531ba574faa561b
Filippo Scaramuzza <filippo@Air-di-Filippo.lan>
2021-02-23 13:54:36 +0100

    Fatto drag and drop ma da terminare il calcolo della dimensione in MB

--- clouest_client/client/src/AddFilePopup.js
+++ clouest_client/client/src/AddFilePopup.js
@@ -3,6 +3,15 @@ import Popup from 'reactjs-popup';
 import "./css/AddFilePopup.css";
 
 class AddFilePopup extends Component {
+  state = {
+    file: null
+  }
+
+  fileInputOnChange = (e) => {
+    console.log(e.target.files[0]);
+    e.target.setAttribute("data-title", e.target.files[0].name + "\n(" + e.target.files[0].size + 0.000001 + " MB)");
+  }
+
   render() {
     return (
       <Popup trigger={<button className="ui teal right labeled icon button"
@@ -11,20 +20,21 @@ class AddFilePopup extends Component {
                 Add File
               </button>} modal>
 
-          <div className="modal">
-            <h3 className="ui horizontal divider header">
-              <i className="teal file icon"></i>
+        <div className="modal">
+          <h3 className="ui horizontal divider header">
+            <i className="teal file icon"></i>
             Add File
           </h3>
-            <form className="ui form">
-              <div class="field">
-                <input type="file" />
+          <form className="ui form">
+              <div className="form-group inputDnD">
+                <input type="file" className="form-control-file text-danger font-weight-bold" id="inputFile" data-title="Drag and drop a file or click here"
+                onChange={this.fileInputOnChange} />
               </div>
-
-                <button className="ui teal right labeled icon button" style={{ margin: "auto", display: "block" }}>
-                  <i className="upload icon"></i>
+              <br/>
+              <button className="ui teal right labeled icon button" style={{ margin: "auto", display: "block" }}>
+                <i className="upload icon"></i>
                 Upload File
-            </button>
+              </button>
           </form>
         </div>
       </Popup>
